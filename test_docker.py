280b117896fa4bfdd7cdd906fcc7045d2157fd40
Leo-nid <leonid.ichalov@gmail.com>
2022-10-12 00:03:49 +0300

    added test for two responses

--- test_docker.py
+++ test_docker.py
@@ -5,4 +5,8 @@ process = subprocess.Popen(['python3', 'server.py'])
 time.sleep(0.2)
 response = requests.post('http://localhost:8888/sort', data=bytes('0 4 1 5 1 5 3', 'utf-8'))
 assert response.content == bytes('0 1 1 3 4 5 5\n', 'utf-8')
+response = requests.post('http://localhost:8888/sort', data=bytes('tasty and dot', 'utf-8'))
+assert response.status_code == 400
+response = requests.get('http://localhost:8888/any_other_request')
+assert response.status_code == 404
 process.terminate()
